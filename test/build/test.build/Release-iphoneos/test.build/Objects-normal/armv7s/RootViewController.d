799fb939e60a3854b1aa6da53f6b1751f3673ee0
evan-lei <dingleiwz0202@163.com>
2015-01-18 19:46:32 +0800

    commit

--- test/build/test.build/Release-iphoneos/test.build/Objects-normal/armv7s/RootViewController.d
+++
@@ -1,2 +0,0 @@
-dependencies: /Users/evan/Desktop/myGit/test/test/RootViewController.m \
-  /Users/evan/Desktop/myGit/test/test/RootViewController.h
--- test/build/test.build/Release-iphoneos/test.build/Objects-normal/armv7s/main.d
+++
@@ -1,2 +0,0 @@
-dependencies: /Users/evan/Desktop/myGit/test/test/main.m \
-  /Users/evan/Desktop/myGit/test/test/AppDelegate.h
